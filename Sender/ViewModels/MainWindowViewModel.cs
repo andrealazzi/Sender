2a988101e95e95994ee9fdfd4960ee54bc249b7e
andrea <andrealazzi@gmail.com>
2020-11-28 12:21:29 +0100

    a

--- Sender/ViewModels/MainWindowViewModel.cs
+++ Sender/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
+using RtMidi.Core;
+using RtMidi.Core.Devices;
+using RtMidi.Core.Enums;
+using RtMidi.Core.Messages;
 
 namespace Sender.ViewModels
 {
@@ -45,41 +49,37 @@ namespace Sender.ViewModels
             int port = Port;
             try
             {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer
-                // connected to the same address as specified by the server, port
-                // combination.
-                TcpClient client = new TcpClient(IP, port);
+                var devices = new List<IMidiInputDevice>();
+                try
+                {
+                    foreach (var inputDeviceInfo in MidiDeviceManager.Default.InputDevices)
+                    {
+                        Console.WriteLine($"Opening {inputDeviceInfo.Name}");
 
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(msg);
+                        var inputDevice = inputDeviceInfo.CreateDevice();
+                        devices.Add(inputDevice);
 
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
+                        //inputDevice.ControlChange += ControlChangeHandler;
+                        inputDevice.Open();
+                       inputDevice.NoteOn += ControlChangeHandler;
+                    }
 
-                NetworkStream stream = client.GetStream();
+                    //Console.WriteLine("Press any key to stop...");
+                    //Console.ReadKey();
 
-                // Send the message to the connected TcpServer.
-                stream.Write(data, 0, data.Length);
-
-                //Console.WriteLine("Sent: {0}", msg);
-
-                // Receive the TcpServer.response.
-
-                // Buffer to store the response bytes.
-                data = new Byte[256];
-
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
-
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                //Console.WriteLine("Received: {0}", responseData);
-
-                // Close everything.
-                stream.Close();
-                client.Close();
+                }
+                catch { }
+                //finally
+                //{
+                //    foreach (var device in devices)
+                //    {
+                //        device.NoteOn -= ControlChangeHandler;
+                //        device.Dispose();
+                //    }
+                //}
+                
+                
+                
             }
             catch (ArgumentNullException e)
             {
@@ -93,7 +93,55 @@ namespace Sender.ViewModels
             //Console.WriteLine("\n Press Enter to continue...");
             //Console.Read();
         }
+        public  void ControlChangeHandler(IMidiInputDevice sender, in NoteOnMessage msg)
+        {
+            //System.IO.File.WriteAllText(@"C:\Users\AndreaLazzi\Downloads\WriteLines.txt", msg.ToString());
+
+            //Console.WriteLine($"[{sender.Name}] ControlChange: Channel:{msg.Channel} Control:{msg.Velocity} Value:{msg.Velocity}");
+            //string msg2 = msg.Key.ToString();
+
+            // Create a TcpClient.
+            // Note, for this client to work you need to have a TcpServer
+            // connected to the same address as specified by the server, port
+            // combination.
+            TcpClient client = new TcpClient(IP, port);
+
+            // Translate the passed message into ASCII and store it as a Byte array.
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(msg.Key.ToString() + "," + msg.Velocity.ToString() + "," + RtMidi.Core.Enums.Channel.Channel1.ToString());
+            NoteOnMessage message = new NoteOnMessage((Channel)Enum.Parse(typeof(Channel), data[2].ToString()), (Key)Enum.Parse(typeof(Key), data[0].ToString()), Convert.ToInt32(data[1]));
+            // Get a client stream for reading and writing.
+            //  Stream stream = client.GetStream();
+
+            NetworkStream stream = client.GetStream();
+
+            // Send the message to the connected TcpServer.
+            stream.Write(data, 0, data.Length);
+
+            //Console.WriteLine("Sent: {0}", msg);
+
+            // Receive the TcpServer.response.
+
+            // Buffer to store the response bytes.
+            data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            //Console.WriteLine("Received: {0}", responseData);
+
+            // Close everything.
+            stream.Close();
+            client.Close();
+
+        } 
     }
+
+   
+
+
 }
 
 
